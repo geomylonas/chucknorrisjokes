5b8c747af934b86c6851f40e2a783b261ab36d42
George Mylonas <geomylonas@gmail.com>
2018-01-07 03:55:51 +0200

    added xml configuration

--- src/main/java/georgemylonas/chucknorrisjokes/ChucknorrisjokesApplication.java
+++ src/main/java/georgemylonas/chucknorrisjokes/ChucknorrisjokesApplication.java
@@ -2,8 +2,10 @@ package georgemylonas.chucknorrisjokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:ChuckConfig.xml")
 public class ChucknorrisjokesApplication {
 
 	public static void main(String[] args) {
